69754fd83e27bb59c2e93de2b57ba26094a89796
Rebecca Zhou <rmz6yx@virginia.edu>
2019-04-06 15:42:30 -0400

    add actual locations to map

--- company.js
+++ company.js
@@ -7,7 +7,31 @@ $(document).ready(function() {
      });
 });
 
-
+function plotPoints(JSONdata) {
+	locations = [];
+	for (i = 0; i < JSONdata.length; i++) {
+		locations.append([JSONdata[i]['latitude'],JSONdata[i]['longitude']])
+	}
+    // locations = [[42, -102], [38, -98]];
+    // console.log(locations.length)
+    for(i = 0; i < locations.length; i++){
+        var myLatLong = new google.maps.LatLng(locations[i][0],locations[i][1]);
+        var thisCircle = new google.maps.Circle({
+            strokeColor: '#FF0000',
+            strokeOpacity: 0.8,
+            strokeWeight: 2,
+            fillColor: '#FF0000',
+            fillOpacity: 0.35,
+            map: map,
+            center: myLatLong,
+            radius: 50000
+            });
+        alert(thisCircle.center);
+        google.maps.event.addListener(thisCircle, 'click', function() {
+            alert(this.center);
+        });
+    }
+};
 function processData(allText) {
     // var record_num = 5;  // or however many elements there are in each row
     // var allTextLines = allText.split(/\r\n|\n/);
@@ -27,6 +51,7 @@ function processData(allText) {
     alert(csvJSON(allText));
     var JSONdata = JSON.parse(csvJSON(allText));
     alert(JSONdata[0]['women (%)']);
+    plotPoints(JSONdata);
 }
 
 function csvJSON(csv){
